2a0901964f23901f6024a427a57e533fe1d36768
Klaus Reimer <k@ailis.de>
2025-01-24 01:23:57 +0100

    Add missing Observable type

--- src/main/utils/types.ts
+++ src/main/utils/types.ts
@@ -3,6 +3,8 @@
  * See LICENSE.md for licensing information.
  */
 
+import type { Observable } from "@kayahr/observable";
+
 import type { JSXElement } from "../JSXElement.js";
 
 /** Plain object recursively only allowing values of type {@link Element}. */
@@ -10,7 +12,7 @@ export interface ElementObject extends Record<string | symbol, Element> {}
 
 /** The JSX element type. */
 export type Element = JSXElement | Node | (() => Element) |  bigint | boolean | null | number | string | symbol | undefined | Element[] | Promise<Element>
-    | ElementObject;
+    | Observable<Element> | ElementObject;
 
 /** Component properties base type. */
 export type Properties = Record<string, unknown>;
